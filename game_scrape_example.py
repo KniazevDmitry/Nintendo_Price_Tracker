7e4ec6003eb0f61abcd3ee027151cadb6aa6b976
Dmitrii Kniazev <dmitriikniazev@gmail.com>
2024-05-28 22:45:05 +0200

    added checks for json fields

--- game_scrape_example.py
+++ game_scrape_example.py
@@ -9,14 +9,20 @@ if response.status_code == 200:
     soup = BeautifulSoup(html_content, 'lxml')
 
     script_tag = soup.find('script', type="application/ld+json")
-    json_data = script_tag.string
-    data = json.loads(json_data)
+    if script_tag:
+        try:
+            json_data = script_tag.string
+            data = json.loads(json_data)
 
-    game_name = data['name']
-    game_price = data['offers']['price']
-    game_currency = data['offers']['priceCurrency']
+            game_name = data.get('name', 'N/A')
+            game_price = data.get('offers', {}).get('price', 'N/A')
+            game_currency = data.get('offers', {}).get('priceCurrency', 'N/A')
 
-    print(game_name)
-    print(f"{game_price} {game_currency}")
+            print(game_name)
+            print(f"{game_price} {game_currency}")
+        except json.JSONDecodeError:
+            print("Error parsing JSON data.")
+    else:
+        print("JSON-LD script tag not found.")
 else:
     print(f"Error: {response.status_code}")
